e58a1c32e7b9da0d53b589bd3277f5400ce10b38
Pihu Thora <pihuthora@gmail.com>
2022-09-16 08:06:40 +0530

    added login using username and password

--- screens/Login/index.tsx
+++ screens/Login/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from 'react'
+import { Alert, Modal, Pressable, View } from "react-native";
 import { User } from './User'
 import {
   LoginSocialGoogle,
@@ -27,7 +28,7 @@ import {
 } from 'react-social-login-buttons'
 import { HomeParamList } from '../../types'
 import { StackNavigationProp } from '@react-navigation/stack'
-import { Button, Text } from 'react-native'
+import { Button, StyleSheet, Text, TextInput } from 'react-native'
 
 interface Props {
   navigation: StackNavigationProp<HomeParamList, 'LoginScreen'>
@@ -44,7 +45,10 @@ function Login({ navigation }: Props) {
   }, [])
 
   const onPressLearnMore = () => {
-    navigation.navigate('HomeScreen')
+    if(username=="admin" && password=="admin")
+      navigation.navigate('HomeScreen')
+    else
+      setModalVisible(true)
   }
 
   const onLogoutSuccess = useCallback(() => {
@@ -55,18 +59,113 @@ function Login({ navigation }: Props) {
 
   const onLogout = useCallback(() => {}, [])
 
+  const styles = StyleSheet.create({
+    baseText: {
+      fontWeight: 'bold',
+      color: 'black',
+      fontSize: 20,
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10
+    },
+    baseHeader: {
+      fontWeight: 'bold',
+      color: 'black',
+      fontSize: 20,
+      height: 40,
+      margin: 12,
+      padding: 10
+    },
+    titleText: {
+      fontSize: 20,
+      fontWeight: "bold"
+    },
+    default: {
+      height: 20,
+      width: 20
+    },
+    baseDiv: {
+      alignItems: 'center'
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
+    buttonOpen: {
+      backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+      backgroundColor: "#2196F3",
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    }
+  });
+  const [username, setUsername] = React.useState("")
+  const [password, setPassword] = React.useState("")
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <>
       {provider != null ? ( profile !=null ? (<User provider={provider} profile={profile} onLogout={onLogout} />):null) :null}
       <div className={`App ${provider && profile ? 'hide' : ''}`}>
-        <Text>ReactJS Social Login</Text>
-
-        <Button
-          onPress={onPressLearnMore}
-          title="Login"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
+          <Text style={styles.baseHeader}>BalanceIt Login</Text><br/>
+          <Text style={styles.default}>Username : </Text>
+          <TextInput style={styles.baseText} value={username} onChangeText={setUsername}></TextInput><br/>
+          <Text style={styles.default}>Password : </Text>
+          <TextInput secureTextEntry={true} style={styles.baseText} value={password} onChangeText={setPassword}></TextInput><br/>
+          <Button 
+              onPress={onPressLearnMore}
+              title="Login"
+              color="#FFA500"
+              accessibilityLabel="Learn more about this purple button"
+            />
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(false);
+            }}
+          >
+            <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Wrong Password!</Text>
+            <Pressable style={[styles.button, styles.buttonOpen]} onPress={() => setModalVisible(false)}>
+            <Text style={styles.textStyle}>Close</Text>
+      </Pressable>
+          </View>
+        </View>
+      </Modal>
 
         <LoginSocialFacebook
           appId={process.env.REACT_APP_FB_APP_ID || ''}
